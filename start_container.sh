68c1c0f0d10ef0320b6e2fd7a6337b8258b959aa
lokesh shanmugam <77161841+Loke7132@users.noreply.github.com>
2025-01-04 02:36:32 -0500

    final-update

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull lokeshshanmugam143/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 lokeshshanmugam143/simple-python-flask-app
